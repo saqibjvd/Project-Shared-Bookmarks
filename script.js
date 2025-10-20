3ad34c11b52cde99ec421d4935ca14a4be999309
saqib javed <70890519+saqibjvd@users.noreply.github.com>
2025-10-20 21:33:38 +0100

    Add heading, form and buttons

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
 import { getUserIds } from "./storage.js";
 
-window.onload = function () {
-  const users = getUserIds();
-  document.querySelector("body").innerText = `There are ${users.length} users`;
-};
+// window.onload = function () {
+//   const users = getUserIds();
+//   document.querySelector("body").innerText = `There are ${users.length} users`;
+// };
